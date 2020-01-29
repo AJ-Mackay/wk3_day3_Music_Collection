2b057e0b1bf66370da53773829f708462156f60f
Ruiner85 <paul.m.mackay85@gmail.com>
2020-01-29 15:04:10 +0000

    Adding instances of classes

--- db/console.rb
+++ db/console.rb
@@ -2,7 +2,19 @@ require('pry')
 require_relative("../models/album")
 require_relative("../models/artist")
 
+artist1=Artist.new({
+  'artist_name' => 'Red Hot Chilli Peppers'
+  })
 
+artist1.save
+
+  album1=Album.new({
+  'album_name' => 'Californication',
+  'genre' => 'Alt Rock',
+  'artist_id'=> artist1.id
+    })
+
+album1.save
 
 binding.pry
 nil
